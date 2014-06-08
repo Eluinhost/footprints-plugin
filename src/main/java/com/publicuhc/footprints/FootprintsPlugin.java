55956afc4560202d21ea75ebcb9c9ff6bb255545
Graham <graham_howden1@yahoo.co.uk>
2014-06-08 19:34:28 +0100

    save configs

--- src/main/java/com/publicuhc/footprints/FootprintsPlugin.java
+++ src/main/java/com/publicuhc/footprints/FootprintsPlugin.java
@@ -1,9 +1,16 @@
 package com.publicuhc.footprints;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FootprintsPlugin extends JavaPlugin {
 
     public static final String LEAVE_FOOTPRINT_PERMISSION = "Footprint.leavePrint";
 
+    @Override
+    public void onEnable() {
+        FileConfiguration configuration = getConfig();
+        configuration.options().copyDefaults(true);
+        saveConfig();
+    }
 }
