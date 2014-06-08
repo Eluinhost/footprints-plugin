185a6f99d900bce686a05b0206135768f60f1fc6
Graham <graham_howden1@yahoo.co.uk>
2014-06-08 19:33:12 +0100

    add the permission

--- src/main/java/com/publicuhc/footprints/FootprintsPlugin.java
+++ src/main/java/com/publicuhc/footprints/FootprintsPlugin.java
@@ -4,4 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class FootprintsPlugin extends JavaPlugin {
 
+    public static final String LEAVE_FOOTPRINT_PERMISSION = "Footprint.leavePrint";
+
 }
